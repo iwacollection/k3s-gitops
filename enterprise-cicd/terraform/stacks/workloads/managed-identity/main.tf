bcab62a8c536956726503117da91372ec4f71e11
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-18 21:38:59 +0800

    style(iac): terraform fmt workload stacks

--- enterprise-cicd/terraform/stacks/workloads/managed-identity/main.tf
+++ enterprise-cicd/terraform/stacks/workloads/managed-identity/main.tf
@@ -1,15 +1,17 @@
 module "resource_group" {
   source = "../../../modules/resource-group"
-  name = var.resource_group_name
+
+  name     = var.resource_group_name
   location = var.location
-  tags = var.tags
+  tags     = var.tags
 }
 
 module "managed_identity" {
   source = "../../../modules/managed-identity"
-  name = var.identity_name
-  resource_group_name = module.resource_group.name
-  location = module.resource_group.location
+
+  name                  = var.identity_name
+  resource_group_name   = module.resource_group.name
+  location              = module.resource_group.location
   federated_credentials = {}
-  tags = var.tags
+  tags                  = var.tags
 }
