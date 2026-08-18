7f927da4b90df2887b69ae9674940fbf31dc5d41
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-18 21:54:29 +0800

    fix(iac): align AKS and private DNS with AzureRM 4.81

--- enterprise-cicd/terraform/modules/private-dns/main.tf
+++ enterprise-cicd/terraform/modules/private-dns/main.tf
@@ -22,8 +22,10 @@ locals {
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   for_each = local.virtual_network_links
 
-  name                 = each.value.link_name
-  private_dns_zone_id  = azurerm_private_dns_zone.this[each.value.zone_name].id
-  virtual_network_id   = each.value.virtual_network_id
-  registration_enabled = false
+  name                  = each.value.link_name
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.this[each.value.zone_name].name
+  virtual_network_id    = each.value.virtual_network_id
+  registration_enabled  = false
+  tags                  = var.tags
 }
