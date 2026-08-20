86a42b2d27fca0e1d4072329dd59e5d5cdd78de4
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:31:33 +0800

    Add database terraform module

---
+++ infrastructure/terraform/modules/database/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_postgresql_flexible_server" "main" {
+  name                   = var.name
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  version                = "16"
+  administrator_login     = var.admin_username
+  administrator_password = var.admin_password
+  storage_mb             = 32768
+  sku_name               = "B_Standard_B1ms"
+}
