03e7df0ba6fd4a0596f215597549614585452774
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:33:14 +0800

    feat(terraform): add managed redis module

---
+++ infrastructure/terraform/modules/managed_redis/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+resource "azurerm_redis_cache" "main" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  capacity            = var.capacity
+  family              = var.family
+  sku_name            = var.sku_name
+  minimum_tls_version = "1.2"
+}
