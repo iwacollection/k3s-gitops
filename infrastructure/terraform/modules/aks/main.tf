f5814cc6e376e18d91256c97ec68e8443e6583bd
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:31:07 +0800

    Add AKS terraform module

---
+++ infrastructure/terraform/modules/aks/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.name
+
+  default_node_pool {
+    name       = "system"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
