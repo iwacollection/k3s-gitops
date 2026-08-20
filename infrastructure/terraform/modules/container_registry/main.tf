f8d62fd85643d25f8178aec4d62a4d9c39ceb7f8
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:29:04 +0800

    Add enterprise terraform acr module

---
+++ infrastructure/terraform/modules/container_registry/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = false
+}
