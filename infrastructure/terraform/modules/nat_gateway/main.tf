199077ce9401520a37a6238341e3aaac31b3e6fc
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 20:04:25 +0800

    feat(terraform): add nat gateway module

---
+++ infrastructure/terraform/modules/nat_gateway/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_nat_gateway" "main" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = "Standard"
+}
