8396dc98cca1f98e89ab00dc1ce2c6252c19a24a
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 01:23:51 +0800

    fix terraform dev network module inputs

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -13,6 +13,25 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "platform" {
+  name     = "rg-platform-cicd"
+  location = "East Asia"
+}
+
 module "network" {
   source = "../../modules/network"
+
+  resource_group_name = azurerm_resource_group.platform.name
+  location            = azurerm_resource_group.platform.location
+
+  vnet_name = "vnet-platform-dev"
+  subnet_name = "snet-platform-dev"
+
+  address_space = [
+    "10.10.0.0/16"
+  ]
+
+  subnet_prefixes = [
+    "10.10.1.0/24"
+  ]
 }
