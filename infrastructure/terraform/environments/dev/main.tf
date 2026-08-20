0e0359b56eed9fed978cddae3d3f9f863dec9f26
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 01:58:51 +0800

    fix terraform apply resource naming drift with existing azure state

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "platform" {
-  name     = "rg-platform-cicd"
+  name     = "rg-iac-dev-platform"
   location = "East Asia"
 }
 
@@ -24,8 +24,8 @@ module "network" {
   resource_group_name = azurerm_resource_group.platform.name
   location            = azurerm_resource_group.platform.location
 
-  vnet_name   = "vnet-platform-dev"
-  subnet_name = "snet-platform-dev"
+  vnet_name   = "vnet-iac-dev"
+  subnet_name = "subnet-platform"
 
   address_space = [
     "10.10.0.0/16"
