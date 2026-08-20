4ccfa15f877a331958f2f68019cdcbd80f2cf0ef
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:24:25 +0800

    feat(iac): add identity module baseline

---
+++ infrastructure/terraform/modules/identity/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+resource "azurerm_user_assigned_identity" "main" {
+  name                = var.identity_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
