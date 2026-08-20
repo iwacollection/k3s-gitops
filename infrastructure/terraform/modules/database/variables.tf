e6e24927606fc08cc76aa33a267322a2fba7092a
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:31:42 +0800

    Add database terraform variables

---
+++ infrastructure/terraform/modules/database/variables.tf
@@ -0,0 +1,5 @@
+variable "name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "admin_username" { type = string }
+variable "admin_password" { type = string }
