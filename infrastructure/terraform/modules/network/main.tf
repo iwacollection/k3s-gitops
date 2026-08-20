3f1264c6423ea67345609724476d20d0eab1084f
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:21:45 +0800

    feat(iac): add network module baseline

---
+++ infrastructure/terraform/modules/network/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_prefixes
+}
