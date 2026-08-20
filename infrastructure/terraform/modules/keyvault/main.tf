a6eede4ccd955547b464308efe82a8f91b539d64
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-20 19:27:35 +0800

    feat(iac): add keyvault module

---
+++ infrastructure/terraform/modules/keyvault/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_key_vault" "main" {
+  name                        = var.name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = true
+}
