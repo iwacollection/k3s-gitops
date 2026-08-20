7ae089038846c3d5fa3c59bbbbf1a99dc4c4c80f
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 00:30:42 +0800

    fix terraform azure backend use azuread auth

--- infra/environments/dev/backend.tf
+++ infra/environments/dev/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "sttfstatec12c3a3699d8"
     container_name       = "tfstate"
     key                  = "dev/platform.tfstate"
+    use_azuread_auth     = true
   }
 }
