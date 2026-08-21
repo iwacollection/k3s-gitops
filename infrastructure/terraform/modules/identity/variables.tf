2de233c25f9987ef5a806d4383f61e4c60817a3e
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 10:05:53 +0800

    harden identity module with RBAC inputs

--- infrastructure/terraform/modules/identity/variables.tf
+++ infrastructure/terraform/modules/identity/variables.tf
@@ -9,3 +9,12 @@ variable "resource_group_name" {
 variable "location" {
   type = string
 }
+
+variable "role_assignments" {
+  description = "Azure RBAC assignments for the managed identity"
+  type = list(object({
+    scope                = string
+    role_definition_name = string
+  }))
+  default = []
+}
