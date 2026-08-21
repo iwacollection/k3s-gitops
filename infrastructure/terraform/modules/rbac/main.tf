f20eca8fe258b580ea7513be4b490ca95a98306d
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 19:07:10 +0800

    feat(terraform): add azure rbac role assignments module

---
+++ infrastructure/terraform/modules/rbac/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = var.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = var.principal_id
+}
+
+resource "azurerm_role_assignment" "keyvault_reader" {
+  scope                = var.keyvault_id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = var.principal_id
+}
+
+lifecycle {
+  prevent_destroy = true
+}
