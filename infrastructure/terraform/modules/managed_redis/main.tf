a57526a295d672acc1ce74f94444ae51a737c158
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:58:46 +0800

    feat(terraform): productionize azure redis resource

--- infrastructure/terraform/modules/managed_redis/main.tf
+++ infrastructure/terraform/modules/managed_redis/main.tf
@@ -10,8 +10,20 @@ resource "azurerm_redis_cache" "main" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
+
   capacity            = var.capacity
   family              = var.family
   sku_name            = var.sku_name
+
   minimum_tls_version = "1.2"
+
+  public_network_access_enabled = false
+
+  redis_configuration {
+    maxmemory_policy = "allkeys-lru"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
