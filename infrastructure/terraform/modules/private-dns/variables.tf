624a7746e5fc5d2162e3d9416ec86990eab94312
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 19:08:44 +0800

    Add private DNS module variables

---
+++ infrastructure/terraform/modules/private-dns/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = string
+}
+
+variable "zone_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "virtual_network_id" {
+  type = string
+}
