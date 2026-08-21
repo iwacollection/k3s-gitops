bd9784af17093fb3a1f11e8761f4fd33a70bf8df
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 00:25:08 +0800

    fix(terraform): format managed redis module

--- infrastructure/terraform/modules/managed-redis/main.tf
+++ infrastructure/terraform/modules/managed-redis/main.tf
@@ -3,9 +3,9 @@ resource "azurerm_redis_cache" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  capacity            = var.capacity
-  family              = "C"
-  sku_name            = var.sku_name
+  capacity = var.capacity
+  family   = "C"
+  sku_name = var.sku_name
 
   non_ssl_port_enabled = false
   minimum_tls_version  = "1.2"
