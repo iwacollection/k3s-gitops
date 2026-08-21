38b6808c14d209eea0ce4ff3bda00ecad049a695
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:46:32 +0800

    feat(terraform): enhance azure monitoring foundation

--- infrastructure/terraform/modules/monitoring/main.tf
+++ infrastructure/terraform/modules/monitoring/main.tf
@@ -12,4 +12,35 @@ resource "azurerm_log_analytics_workspace" "main" {
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
   retention_in_days   = var.retention_days
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_monitor_workspace" "main" {
+  name                = "${var.name}-prometheus"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    managed_by = "terraform"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_dashboard_grafana" "main" {
+  name                = "${var.name}-grafana"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  api_key_enabled = false
+  deterministic_outbound_ip_enabled = false
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
