b5d9a674470bf0d69e9271b2ebab5146478d9342
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:42:45 +0800

    feat(terraform): expand private dns module for azure services

--- infrastructure/terraform/modules/private_dns/main.tf
+++ infrastructure/terraform/modules/private_dns/main.tf
@@ -1,11 +1,19 @@
-resource "azurerm_private_dns_zone" "main" {
-  name                = var.zone_name
+resource "azurerm_private_dns_zone" "zones" {
+  for_each = toset(var.private_dns_zones)
+
+  name                = each.value
   resource_group_name = var.resource_group_name
+
+  tags = var.tags
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "main" {
-  name                  = "${var.zone_name}-link"
+resource "azurerm_private_dns_zone_virtual_network_link" "links" {
+  for_each = azurerm_private_dns_zone.zones
+
+  name                  = "${replace(each.key, ".", "-")}-link"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.main.name
+  private_dns_zone_name = each.value.name
   virtual_network_id    = var.virtual_network_id
+
+  registration_enabled = false
 }
