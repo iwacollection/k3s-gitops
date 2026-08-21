6c059b8c65f4f98241917335dbe23b05ed1165a9
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:43:40 +0800

    feat(terraform): add private endpoint dns variables

---
+++ infrastructure/terraform/modules/private_endpoint/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "resource_id" {
+  type = string
+}
+
+variable "private_dns_zone_ids" {
+  type    = list(string)
+  default = []
+}
