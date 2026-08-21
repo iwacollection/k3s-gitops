2039712d093dfa026705baaed1bf6e36ef522f85
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:38:58 +0800

    feat(terraform): harden postgres module production baseline

--- infrastructure/terraform/modules/database/main.tf
+++ infrastructure/terraform/modules/database/main.tf
@@ -5,10 +5,17 @@ resource "azurerm_postgresql_flexible_server" "main" {
   version                = "16"
   administrator_login     = var.admin_username
   administrator_password = var.admin_password
-  storage_mb             = 32768
-  sku_name               = "B_Standard_B1ms"
 
-  backup_retention_days = 7
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms"
+
+  backup_retention_days = 14
+
+  public_network_access_enabled = false
+
+  high_availability {
+    mode = "ZoneRedundant"
+  }
 
   lifecycle {
     prevent_destroy = true
