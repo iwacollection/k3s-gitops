5741313847e8b670106c3ffa8aa43b2f40ab12c6
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 21:10:15 +0800

    feat(terraform): add load balancer ports and probe variables

--- infrastructure/terraform/modules/load_balancer/variables.tf
+++ infrastructure/terraform/modules/load_balancer/variables.tf
@@ -3,4 +3,7 @@ variable "location" { type = string }
 variable "resource_group_name" { type = string }
 variable "subnet_id" { type = string }
 variable "sku" { type = string default = "Standard" }
+variable "frontend_port" { type = number default = 80 }
+variable "backend_port" { type = number default = 80 }
+variable "probe_port" { type = number default = 80 }
 variable "tags" { type = map(string) default = {} }
