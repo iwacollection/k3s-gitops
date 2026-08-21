cf415ecb7efcaf60accc6572ff5930d7aafe1b6d
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 10:21:35 +0800

    feat(terraform): configure AKS maintenance window baseline

--- infrastructure/terraform/modules/aks/main.tf
+++ infrastructure/terraform/modules/aks/main.tf
@@ -21,16 +21,23 @@ resource "azurerm_kubernetes_cluster" "main" {
   oidc_issuer_enabled                = var.oidc_issuer_enabled
 
   default_node_pool {
-    name                 = "system"
-    node_count           = var.node_count
-    vm_size              = var.vm_size
-    enable_auto_scaling  = var.enable_auto_scaling
-    min_count            = var.min_count
-    max_count            = var.max_count
-    availability_zones   = var.availability_zones
+    name                = "system"
+    node_count          = var.node_count
+    vm_size             = var.vm_size
+    enable_auto_scaling = var.enable_auto_scaling
+    min_count           = var.min_count
+    max_count           = var.max_count
+    availability_zones  = var.availability_zones
 
     upgrade_settings {
       max_surge = var.max_surge
+    }
+  }
+
+  maintenance_window {
+    allowed {
+      day   = var.maintenance_window_day
+      hours = [2]
     }
   }
 
