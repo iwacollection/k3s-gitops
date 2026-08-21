e15f5a49094e7faf92286405106ac14180ad6106
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:35:14 +0800

    complete azure network module with nat gateway and nsg foundation

--- infrastructure/terraform/modules/network/main.tf
+++ infrastructure/terraform/modules/network/main.tf
@@ -40,3 +40,39 @@ resource "azurerm_subnet" "private_endpoint" {
     prevent_destroy = true
   }
 }
+
+resource "azurerm_public_ip" "nat" {
+  name                = "${var.vnet_name}-nat-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                = "${var.vnet_name}-nat"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "main" {
+  subnet_id      = azurerm_subnet.main.id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.vnet_name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
