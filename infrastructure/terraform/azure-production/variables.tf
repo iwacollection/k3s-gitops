1bfb6683e3da4e7d8d0b8444dfe8cc503cb2ac8e
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 14:53:13 +0800

    feat: add azure production terraform variables

---
+++ infrastructure/terraform/azure-production/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+  default = "eastus"
+}
+
+variable "address_space" {
+  type = list(string)
+  default = ["10.10.0.0/16"]
+}
