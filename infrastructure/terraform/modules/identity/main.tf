3e130c9efd7bae11ce4cf77d6877dfc9593a2688
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 09:43:32 +0800

    harden identity module with prevent_destroy lifecycle

--- infrastructure/terraform/modules/identity/main.tf
+++ infrastructure/terraform/modules/identity/main.tf
@@ -10,4 +10,8 @@ resource "azurerm_user_assigned_identity" "main" {
   name                = var.identity_name
   resource_group_name = var.resource_group_name
   location            = var.location
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
