c847578e230f4ec0e03e0d2666ff12b4a6fc6d58
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-21 20:53:42 +0800

    feat(terraform): enhance azure load balancer module

--- infrastructure/terraform/modules/load_balancer/main.tf
+++ infrastructure/terraform/modules/load_balancer/main.tf
@@ -10,6 +10,13 @@ resource "azurerm_lb" "main" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
+  sku                 = var.sku
 
-  sku = "Standard"
+  frontend_ip_configuration {
+    name                          = "frontend"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = var.tags
 }
