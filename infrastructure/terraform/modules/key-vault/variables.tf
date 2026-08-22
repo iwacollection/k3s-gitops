56560859508ac54c1e037a08161fa51647bfbcbe
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 14:41:26 +0800

    feat(terraform): add Key Vault module variables

---
+++ infrastructure/terraform/modules/key-vault/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "soft_delete_retention_days" {
+  type    = number
+  default = 7
+}
+
+variable "purge_protection_enabled" {
+  type    = bool
+  default = false
+}
