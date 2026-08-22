6f30d50130e3c1566631aaabfb826e2cc6977959
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 15:09:27 +0800

    feat(keyvault): enable purge protection and private-only access

--- infrastructure/terraform/modules/key-vault/main.tf
+++ infrastructure/terraform/modules/key-vault/main.tf
@@ -1,14 +1,13 @@
 resource "azurerm_key_vault" "this" {
-  name                       = var.name
-  location                   = var.location
-  resource_group_name        = var.resource_group_name
-  tenant_id                  = var.tenant_id
-  sku_name                   = "standard"
-  rbac_authorization_enabled = true
-  soft_delete_retention_days = var.soft_delete_retention_days
-  purge_protection_enabled   = var.purge_protection_enabled
-
-  public_network_access_enabled = true
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tenant_id                     = var.tenant_id
+  sku_name                      = "standard"
+  rbac_authorization_enabled    = true
+  soft_delete_retention_days    = var.soft_delete_retention_days
+  purge_protection_enabled      = var.purge_protection_enabled
+  public_network_access_enabled = var.public_network_access_enabled
 }
 
 output "id" {
