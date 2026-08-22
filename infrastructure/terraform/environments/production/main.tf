6dfef2e7b6355eb6475b35cbfae970cd779951b5
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 16:31:10 +0800

    feat(terraform): wire private dns module into production root

--- infrastructure/terraform/environments/production/main.tf
+++ infrastructure/terraform/environments/production/main.tf
@@ -8,6 +8,25 @@ variable "resource_group_name" {
   default = "rg-k3s-production"
 }
 
+variable "virtual_network_id" {
+  type = string
+}
+
+module "private_dns" {
+  source = "../../modules/private-dns"
+
+  name_prefix          = "k3s-production"
+  resource_group_name = var.resource_group_name
+  virtual_network_id  = var.virtual_network_id
+
+  zones = [
+    "privatelink.azurecr.io",
+    "privatelink.vaultcore.azure.net",
+    "privatelink.redis.cache.windows.net",
+    "privatelink.postgres.database.azure.com"
+  ]
+}
+
 module "load_balancer" {
   source = "../../modules/load-balancer"
 
