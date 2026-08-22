15eab08a45e32ae6eb9b731fe03406745a032d7f
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 14:40:43 +0800

    feat(terraform): type NAT gateway variables

--- infrastructure/terraform/modules/nat-gateway/variables.tf
+++ infrastructure/terraform/modules/nat-gateway/variables.tf
@@ -1,3 +1,16 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "idle_timeout_in_minutes" {
+  type    = number
+  default = 10
+}
