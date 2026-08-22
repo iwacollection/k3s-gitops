61519ce9a2408b5a8ea4a3d37cbfcaf2363aa950
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 13:58:54 +0800

    fix(terraform): configure PostgreSQL in East US 2

--- infrastructure/terraform/environments/production/database_variables.tf
+++ infrastructure/terraform/environments/production/database_variables.tf
@@ -7,3 +7,13 @@ variable "database_admin_password" {
   type      = string
   sensitive = true
 }
+
+variable "database_location" {
+  type    = string
+  default = "East US 2"
+}
+
+variable "database_sku_name" {
+  type    = string
+  default = "GP_Standard_D2s_v3"
+}
