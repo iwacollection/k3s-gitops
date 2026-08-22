12bd8e3f032bc5d1486364315661afdee67db769
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-23 00:31:03 +0800

    feat: add application gateway waf foundation module

---
+++ infrastructure/terraform/modules/application-gateway-waf/main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_public_ip" "this" {
+  name                = "${var.name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_application_gateway" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "gateway-ip-config"
+    subnet_id = var.subnet_id
+  }
+
+  frontend_port {
+    name = "https"
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = "public"
+    public_ip_address_id = azurerm_public_ip.this.id
+  }
+
+  waf_configuration {
+    enabled          = true
+    firewall_mode    = "Prevention"
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
+  lifecycle {
+    ignore_changes = [backend_address_pool]
+  }
+}
