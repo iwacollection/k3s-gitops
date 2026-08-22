2ffb6210e1ce6febacdf9058c1d575fd14221230
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 14:40:50 +0800

    feat(terraform): add ACR module

---
+++ infrastructure/terraform/modules/container-registry/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_container_registry" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = false
+}
+
+output "id" {
+  value = azurerm_container_registry.this.id
+}
+
+output "login_server" {
+  value = azurerm_container_registry.this.login_server
+}
+
+output "name" {
+  value = azurerm_container_registry.this.name
+}
