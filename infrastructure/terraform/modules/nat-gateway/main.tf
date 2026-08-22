0e7303c600f2f15aeb66e81b77c2785c5b2af6c7
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 14:40:27 +0800

    feat(terraform): complete NAT gateway module

--- infrastructure/terraform/modules/nat-gateway/main.tf
+++ infrastructure/terraform/modules/nat-gateway/main.tf
@@ -1,10 +1,32 @@
-resource "azurerm_nat_gateway" "main" {
-  name                = var.name
+resource "azurerm_public_ip" "this" {
+  name                = "${var.name}-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = "Standard"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "this" {
+  name                    = var.name
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = var.idle_timeout_in_minutes
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "this" {
+  nat_gateway_id       = azurerm_nat_gateway.this.id
+  public_ip_address_id = azurerm_public_ip.this.id
 }
 
 output "id" {
-  value = azurerm_nat_gateway.main.id
+  value = azurerm_nat_gateway.this.id
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.this.ip_address
+}
+
+output "public_ip_id" {
+  value = azurerm_public_ip.this.id
 }
