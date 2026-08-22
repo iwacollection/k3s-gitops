89aa782d146352c0bd40568723dafcc2cc4a0d12
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-22 14:44:31 +0800

    fix(terraform): use current Key Vault RBAC argument

--- infrastructure/terraform/modules/key-vault/main.tf
+++ infrastructure/terraform/modules/key-vault/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault" "this" {
   resource_group_name        = var.resource_group_name
   tenant_id                  = var.tenant_id
   sku_name                   = "standard"
-  enable_rbac_authorization  = true
+  rbac_authorization_enabled = true
   soft_delete_retention_days = var.soft_delete_retention_days
   purge_protection_enabled   = var.purge_protection_enabled
 
