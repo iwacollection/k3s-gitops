5c6a9a1499beadab163842f95b67026a308546d3
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-23 09:48:20 +0800

    add production database private endpoint variables

--- infrastructure/terraform/environments/production/database_variables.tf
+++ infrastructure/terraform/environments/production/database_variables.tf
@@ -17,3 +17,18 @@ variable "database_sku_name" {
   type    = string
   default = "GP_Standard_D2s_v3"
 }
+
+variable "database_private_endpoint_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "database_private_endpoint_subnet_id" {
+  type    = string
+  default = null
+}
+
+variable "database_private_dns_zone_ids" {
+  type    = list(string)
+  default = []
+}
