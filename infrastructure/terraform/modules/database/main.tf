8185464df122537ca5108cc0d528908087c98719
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-23 09:45:29 +0800

    hardening: add postgres private endpoint wiring

--- infrastructure/terraform/modules/database/main.tf
+++ infrastructure/terraform/modules/database/main.tf
@@ -15,3 +15,28 @@ resource "azurerm_postgresql_flexible_server" "this" {
   geo_redundant_backup_enabled  = true
   public_network_access_enabled = var.public_network_access_enabled
 }
+
+resource "azurerm_private_endpoint" "postgres" {
+  count = var.private_endpoint_enabled ? 1 : 0
+
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-postgres-connection"
+    private_connection_resource_id = azurerm_postgresql_flexible_server.this.id
+    subresource_names              = ["postgresqlServer"]
+    is_manual_connection           = false
+  }
+
+  dynamic "private_dns_zone_group" {
+    for_each = length(var.private_dns_zone_ids) > 0 ? [1] : []
+
+    content {
+      name                 = "default"
+      private_dns_zone_ids = var.private_dns_zone_ids
+    }
+  }
+}
