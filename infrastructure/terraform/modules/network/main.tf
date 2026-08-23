7a8615ef1044e94ffbfd7283e323d967a2c7dcf3
iwacollection <102014572+iwacollection@users.noreply.github.com>
2026-08-23 10:04:51 +0800

    Add network NSG associations for subnet hardening

--- infrastructure/terraform/modules/network/main.tf
+++ infrastructure/terraform/modules/network/main.tf
@@ -5,6 +5,24 @@ resource "azurerm_virtual_network" "this" {
   address_space       = var.address_space
 }
 
+resource "azurerm_network_security_group" "aks" {
+  name                = "${var.name}-aks-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_network_security_group" "private_endpoints" {
+  name                = "${var.name}-private-endpoints-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_network_security_group" "ingress" {
+  name                = "${var.name}-ingress-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_subnet" "aks" {
   name                 = "${var.name}-aks"
   resource_group_name  = var.resource_group_name
@@ -30,4 +48,19 @@ resource "azurerm_subnet" "ingress" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = var.ingress_subnet_prefixes
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks" {
+  subnet_id                 = azurerm_subnet.aks.id
+  network_security_group_id = azurerm_network_security_group.aks.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private_endpoints" {
+  subnet_id                 = azurerm_subnet.private_endpoints.id
+  network_security_group_id = azurerm_network_security_group.private_endpoints.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "ingress" {
+  subnet_id                 = azurerm_subnet.ingress.id
+  network_security_group_id = azurerm_network_security_group.ingress.id
 }
